8d10ba63a9e486891457aab7810e8b4f0329f0c2
terranandes <terranandes@gmail.com>
2025-07-15 19:04:59 +0800

    fixed integer to strong cast compilation error

--- project_tw/run_all.py
+++ project_tw/run_all.py
@@ -9,21 +9,24 @@ For example:\n\
 run_all.py 2025\
 '
 
-end_year_in = '2025'
+end_year_in = 2025
 if (len(sys.argv) > 1 and sys.argv[1] == '-h') or len(sys.argv) > 2:
     print(help_msg)
     exit()
 
 if len(sys.argv) == 2:
-    end_year_in = sys.argv[1]
+    try:
+        end_year_in = int(sys.argv[1])
+    except:
+        print("Input end_year is not a valid integer!")
 
 #Async gathering data
-subprocess.run(['python3', 'main_async_httpx.py', end_year_in])
+subprocess.run(['python3', 'main_async_httpx.py', str(end_year_in)])
 
-subprocess.run('cp -rf stock_list.csv stock_list_s2006e'+end_year_in+'_unfiltered.csv', shell=True)
+subprocess.run('cp -rf stock_list.csv stock_list_s2006e'+str(end_year_in)+'_unfiltered.csv', shell=True)
 
 #Filtering data
-subprocess.run(['python3', 'pandas_stock.py', end_year_in])
+subprocess.run(['python3', 'pandas_stock.py', str(end_year_in)])
 
 #Predicting step to next
 
