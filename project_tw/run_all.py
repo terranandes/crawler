439108699e29336cc15b6f91c812c4b970f451e9
terranandes <terranandes@gmail.com>
2025-07-15 22:47:47 +0800

    enable all

--- project_tw/run_all.py
+++ project_tw/run_all.py
@@ -21,10 +21,10 @@ if len(sys.argv) == 2:
         print("Input end_year is not a valid integer!")
 
 #Async gathering data
-#subprocess.run(['python3', 'main_async_httpx.py', str(end_year_in)])
+subprocess.run(['python3', 'main_async_httpx.py', str(end_year_in)])
 
-#subprocess.run('rm -f stock_list_s2006e'+str(end_year_in)+'_unfiltered.csv', shell=True)
-#subprocess.run('cp -f stock_list.csv stock_list_s2006e'+str(end_year_in)+'_unfiltered.csv', shell=True)
+subprocess.run('rm -f stock_list_s2006e'+str(end_year_in)+'_unfiltered.csv', shell=True)
+subprocess.run('cp -f stock_list.csv stock_list_s2006e'+str(end_year_in)+'_unfiltered.csv', shell=True)
 
 #Filtering data
 subprocess.run(['python3', 'pandas_stock.py', str(end_year_in)])
